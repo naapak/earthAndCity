e39cd3ff53350ebdf7b6d01ba3319c0c57242983
jzarebski <jzarebski84@gmail.com>
2017-03-17 13:15:00 -0400

    001 updated config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'earthandcity');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'earthandcity');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
