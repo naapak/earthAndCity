327348e60475feb23ac88746ef257e0f68ae8a08
KJ-B <donkjb@Kevins-MacBook-Pro.local>
2017-03-20 16:19:33 -0400

    002
    Pushing 20/03/17 changes to Master.  I worked in the drop down menu in
    wordpress

--- wp-content/themes/earthandcity/template-custom.php
+++ wp-content/themes/earthandcity/template-custom.php
@@ -2,13 +2,13 @@
 
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: About Us
 
  */
 ?>
 
 <?php while (have_posts()) : the_post(); 
-get_the_post_thumbnail_url("assets/imageamages/mamaearth10.jpg");
+get_the_post_thumbnail_url("assets/images/mamaearth10.jpg");
 get_the_post_thumbnail_caption("Lisa Sweetman (right) and Cassandra Rizzotto (left) Co-founders of Earth & City");
 ?>
   <?php get_template_part('templates/page', 'header'); ?>
--- wp-content/themes/earthandcity/templates/custom_page.php
+++ wp-content/themes/earthandcity/templates/custom_page.php
@@ -1,6 +1,8 @@
+
+
 <?php
 
-/*This template is: page-about.php*/
+/*This template is: Our Story
 
 
 is_custom_page( array( 'Our Mission' ,'About', 'In The Press' ) ) ;
@@ -8,5 +10,5 @@ is_custom_page( array( 'Our Mission' ,'About', 'In The Press' ) ) ;
 if ( is_custom_page() && $post->post_parent > 0 ) { 
     echo "This is a child custom_page";
 };
-
+*/
 ?>
