ac59809d21711db758a649af42a57bec390d189d
allenca <allen.ca.chan@gmail.com>
2017-03-30 16:35:56 -0400

    016
    pull request to Naresh to resolve git merge conflicts on his side

--- wp-content/themes/earthandcity/templates/footer.php
+++ wp-content/themes/earthandcity/templates/footer.php
@@ -28,7 +28,7 @@
 
 		<?php dynamic_sidebar( 'footer' ); ?>
 
-		<div class="flexAlignRight">
+		<div class="flexRight">
 		  <a class="iconMargin" href="https://www.facebook.com/earthandcity/"><img src="<?php echo get_bloginfo("stylesheet_directory")?>/assets/images/IconsPNG/facebook-logo-button.png" alt="Facebook Link" width="42" height="42" border="0"></a>
 		  <a class="iconMargin" href="https://www.instagram.com/earthandcity/?hl=en"><img src="<?php echo get_bloginfo("stylesheet_directory")?>/assets/images/IconsPNG/instagram.png" alt="Instagram Link" width="42" height="42" border="0"></a>
 		  <a class="iconMargin" href="https://twitter.com/earthandcity?lang=en"><img src="<?php echo get_bloginfo("stylesheet_directory")?>/assets/images/IconsPNG/twitter-logo-button.png" alt="Twitter Link" width="42" height="42" border="0"></a>
